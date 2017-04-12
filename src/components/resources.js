f91be79da5ca5c5841e363b2a721c439e3604f39
AnnaRokalo <rokalo.a.s@gmail.com>
2017-04-12 15:13:03 +0300

    Create small app with HOC.

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default () => {
+  return (
+    <div>
+      Super special recipe
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup peper</li>
+        <li>1 cup salt</li>
+      </ul>
+    </div>
+  );
+};
